0735e5e39c7dd1b1906fae26ed268d79b0c8b563
Rupesh kumar shaw <shawrupesh020@gmail.com>
2021-10-23 04:07:24 +0530

    Logo added

--- src/Component/Api/Api.js
+++ src/Component/Api/Api.js
@@ -2,30 +2,42 @@
 import React from 'react'
 import './Api.css'
 import { useEffect,useState } from 'react'
-
+let link='https://assets.coincap.io/assets/icons/eth@1x.png'
    export function Api()  {
    const [result, setresult] = useState([])   
    const[count,setCount]=useState(10)
-
-   function handleCount(){
  
-        setCount(count+10)
-   }
-   
 
-      useEffect(() => {
+useEffect(() => {
  fetch('https://api.coincap.io/v2/assets').then((res)=>res.json()).then((data)=>{
-  // console.log(data)
    setresult(data)
- })
-       
- }, [])
+ })     
+ },[])
+
+ function resultReturn(){
+  return result.data  && result.data.slice(0,count).map((item,index)=>{
+    const {rank,name,priceUsd,marketCapUsd,vwap24Hr,supply,volumeUsd24Hr,changePercent24Hr,symbol}=item
+   let sym=symbol.toLowerCase()
+      return(
+       <tr key={index} >
+                <td>{rank}</td>
+               <td><img className="img" src={`https://assets.coincap.io/assets/icons/${sym}@2x.png`} /> {name}</td>
+                <td>${Math.round((priceUsd ) * 100) / 100}</td>
+                <td>${ Math.round((marketCapUsd ) * 100) / 100}</td>
+                <td>${Math.round((vwap24Hr ) * 100) / 100}</td>
+                <td>{ Math.round((supply ) * 100) / 100}</td>
+                <td>${Math.round((volumeUsd24Hr ) * 100) / 100 }</td>
+                <td>{changePercent24Hr}%</td>
+                </tr> )
+   })
+
+ }
+
 
     return (
         <div>
-          
            <table className='students'>
-       <tbody>
+        <tbody>
    <tr>
     <th>Rank </th>
     <th>Name</th>
@@ -36,24 +48,10 @@ import { useEffect,useState } from 'react'
     <th>Volume</th>
     <th>Change</th>
   </tr>
-  {result.data  && result.data.slice(0,count).map((item,index)=>{
-     console.log(item)
-     return(
-      <tr key={index} >
-               <td>{item.rank}</td>
-               <td>{item.name}</td>
-               <td>${Math.round((item.priceUsd ) * 100) / 100}</td>
-               <td>${ Math.round((item.marketCapUsd ) * 100) / 100}</td>
-               <td>${Math.round((item.vwap24Hr ) * 100) / 100}</td>
-               <td>{ Math.round((item.supply ) * 100) / 100}</td>
-               <td>${Math.round((item.volumeUsd24Hr ) * 100) / 100 }</td>
-               <td>{item.changePercent24Hr}%</td>
-               </tr> )
-  })}
-            
-              </tbody>
-           </table>
-           <button onClick={handleCount} className="btn">View More</button>
+  { resultReturn()}
+  </tbody>
+    </table>
+ <button onClick={()=>{setCount(count+10)}} className="btn">View More</button>
             
         </div>
     )
